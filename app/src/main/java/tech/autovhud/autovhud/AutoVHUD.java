91cfcb083b380d1d0dcf0819eb4f9ba1a5ecb8ee
Michael Peng <michael@mcmoo.org>
2018-12-01 18:25:42 -0500

    Add Map Activity

--- app/src/main/java/tech/autovhud/autovhud/AutoVHUD.java
+++ app/src/main/java/tech/autovhud/autovhud/AutoVHUD.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import com.google.android.gms.maps.SupportMapFragment;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e. status bar and
@@ -90,21 +91,6 @@ public class AutoVHUD extends AppCompatActivity {
     setContentView(R.layout.activity_auto_vhud);
 
     mVisible = true;
-    mControlsView = findViewById(R.id.fullscreen_content_controls);
-    mContentView = findViewById(R.id.fullscreen_content);
-
-    // Set up the user interaction to manually show or hide the system UI.
-    mContentView.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        toggle();
-      }
-    });
-
-    // Upon interacting with UI controls, delay any scheduled hide()
-    // operations to prevent the jarring behavior of controls going away
-    // while interacting with the UI.
-    findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
   }
 
   @Override
@@ -115,14 +101,6 @@ public class AutoVHUD extends AppCompatActivity {
     // created, to briefly hint to the user that UI controls
     // are available.
     delayedHide(100);
-  }
-
-  private void toggle() {
-    if (mVisible) {
-      hide();
-    } else {
-      show();
-    }
   }
 
   private void hide() {
