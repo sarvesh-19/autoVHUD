fca602114867ff5d4acc67b2948f5c0b03145a80
Michael Peng <michael@mcmoo.org>
2018-12-01 18:27:17 -0500

    Add rest of new activity

---
+++ app/src/main/java/tech/autovhud/autovhud/DiscreteMapView.java
@@ -0,0 +1,20 @@
+package tech.autovhud.autovhud;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class DiscreteMapView extends AppCompatActivity implements OnMapReadyCallback {
+
+  @Override
+  protected void onCreate(Bundle bundle) {
+    super.onCreate(bundle);
+
+  }
+
+  @Override
+  public void onMapReady(GoogleMap googleMap) {
+
+  }
+}
